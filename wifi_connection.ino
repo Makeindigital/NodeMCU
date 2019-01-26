79d88f941b2f1880b7ab8e7f7525d3da90ac673f
Makeindigital <47047428+Makeindigital@users.noreply.github.com>
2019-01-26 07:53:41 +0530

    Add files via upload

---
+++ wifi_connection.ino
@@ -0,0 +1,34 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid = "xxxxxxxxx";
+const char* password = "xxxxxxxxx";
+
+WiFiServer server(80);
+
+void setup() {
+  Serial.begin(9600);
+  delay(10);
+  
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  server.begin();
+  Serial.println("Server started");
+  Serial.print("Use this URL to connect: ");
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+}
+ 
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
