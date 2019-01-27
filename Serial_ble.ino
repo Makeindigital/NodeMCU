e6b13327b6c26de00d48bebb19bef8d5d2aa8c48
Makeindigital <47047428+Makeindigital@users.noreply.github.com>
2019-01-27 08:55:18 +0530

    Add files via upload

---
+++ Serial_ble.ino
@@ -0,0 +1,45 @@
+ char p;
+ void setup()
+{
+  Serial.begin(9600);
+ 
+}
+void loop()
+{
+  while (Serial.available()==0) { //wait for user input
+}
+ 
+p=Serial.read(); //read user input into pos
+    if(p=='F')
+    {  
+      Serial.println("ROBOT FORWARD");   
+     
+    }
+    if(p=='B')
+    {
+        Serial.println("ROBOT BACKWARD");
+        
+    }
+      if(p=='L')
+    {
+       Serial.println("ROBOT LEFT");
+       
+     
+    }
+    if(p=='R')
+    {
+      Serial.println("ROBOT RIGHT");
+      
+    }
+     if(p=='S')
+    {
+      Serial.println("STOP");
+      
+    }
+    else{
+      Serial.println(p);
+    }
+  }
+
+
+
